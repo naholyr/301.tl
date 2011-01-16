394eaa34003c8e26c58d5bbb28fcf910163cfe92
Nicolas Chambrier <naholyr@gmail.com>
2011-01-16 01:24:27 +0100

    * Basic form for shortening URLs * Fixed static paths (had to remove the catch-all 404)

--- conf/common.js
+++ conf/common.js
@@ -1,7 +1,6 @@
 exports.conf = {
 	port:         3000,                          // Server port
 	host:         '127.0.0.1',                   // Listen connections from
-	static_paths: [process.cwd() + '/public'],   // Static paths
 
 	url_provider: 'redis', // URL provider engine
 	redis: {
